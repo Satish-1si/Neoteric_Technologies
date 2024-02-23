ceff7b21c830b69916530e10f176d663c51d3588
Mohankrish007 <mohangarnepudi2001@gmail.com>
2024-02-23 06:36:36 +0530

    about page route added

--- src/pages/Routes/router.js
+++ src/pages/Routes/router.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import About from '../About/About.js'
 const HomeComponent=React.lazy(()=>import("../Home/Home.js"))
 export const Routers = () => {
   return (
@@ -8,7 +9,7 @@ export const Routers = () => {
       <Route path="services" element={<h1>services</h1>}> </Route>
       <Route path="industries" element={<h1>industries</h1>}> </Route>
       <Route path="careers" element={<h1>careers</h1>}> </Route>
-      <Route path="about" element={<h1>about</h1>}> </Route>
+      <Route path="about" element={<About/>}> </Route>
       <Route path="contact" element={<h1>contact</h1>}> </Route>
       <Route path="News_Blog" element={<h1>News_Blog</h1>}> </Route>
     </Routes>
